afcb10795d48944873dc7db85ab60fc35d39c7d2
GARSON ZHANG (ITA-ISDC-ISD-OOCLL/ZHA) <ZHANGGA5@OOCL.COM>
2019-07-16 08:35:21 +0800

    Refactor: Naming is not standardized in class OrderReceipt/printNameAndAddress printsLineItem

--- src/main/java/org/katas/refactoring/OrderReceipt.java
+++ src/main/java/org/katas/refactoring/OrderReceipt.java
@@ -7,11 +7,11 @@ package org.katas.refactoring;
  * total sales tax) and prints it.
  */
 public class OrderReceipt {
-    private Order o;
+    private Order order;
     private double saleTaxRate = 0.10;
 
-    public OrderReceipt(Order o) {
-        this.o = o;
+    public OrderReceipt(Order order) {
+        this.order = order;
     }
 
     public String printReceipt() {
@@ -29,14 +29,14 @@ public class OrderReceipt {
     }
 
     private void printNameAndAddress(StringBuilder output){
-        output.append(o.getCustomerName());
-        output.append(o.getCustomerAddress());
+        output.append(order.getCustomerName());
+        output.append(order.getCustomerAddress());
     }
 
     private void printsLineItem(StringBuilder output){
         double totSalesTx = 0d;
         double tot = 0d;
-        for (LineItem lineItem : o.getLineItems()) {
+        for (LineItem lineItem : order.getLineItems()) {
             output.append(lineItem.getDescription());
             output.append('\t');
             output.append(lineItem.getPrice());
